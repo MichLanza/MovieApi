29570d62656d51ea3bc0524348e854de3d28583e
Michele Lanza <michele_lanza1@hotmail.com>
2024-08-14 18:45:46 -0400

    custom controller

---
+++ MovieApi/Controllers/CustomBaseController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Db;
+
+namespace MovieApi.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CustomBaseController(AppDbContext context,
+            IMapper mapper
+           )
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        protected async Task<List<TDTO>> Get<TEntity, TDTO>() where TEntity : class
+        {
+            var entities = await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+            var dtos = _mapper.Map<List<TDTO>>(entities);
+            return dtos;
+        }    
+    }
+}
